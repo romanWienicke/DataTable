6e32adef37e4e5da2417aceaf2887952a8a4ddb3
Roman Wienicke <r.wienicke@connexion.at>
2019-05-27 15:02:34 +0200

    Updates DataTable Tests

--- DataTable/DataTable2.cs
+++ DataTable/DataTable2.cs
@@ -9,12 +9,12 @@ using System.Threading.Tasks;
 
 namespace Connexion.DataExtensions
 {
-    public class DataTable2 : MarshalByValueComponent
+    public class DataTable2
     {
         const string NotInitializedExceptionMessage = "Table was not initialized";
 
-        private HashSet<object[]> _data;
-        public HashSet<object[]> Data
+        private HashSet<ColumnData[]> _data;
+        public HashSet<ColumnData[]> Data
         {
             get
             {
@@ -29,7 +29,7 @@ namespace Connexion.DataExtensions
         public DataTable2()
         {
             
-            _data = new HashSet<object[]>();
+            _data = new HashSet<ColumnData[]>();
         }
 
         public static async Task<DataTable2> FromSqlCommand(SqlCommand command)
@@ -67,18 +67,20 @@ namespace Connexion.DataExtensions
             {
                 while (reader.Read())
                 {
-                    var row = new object[columnCount];
+                    var row = new ColumnData[columnCount];
                     for (int columnIndex = 0; columnIndex < columnCount; columnIndex++)
                     {
-                        row[columnIndex] = reader.GetValue(columnIndex);
-                        
+                        row[columnIndex] = new ColumnData {
+                            Column = Columns[columnIndex],
+                            Data = reader.GetValue(columnIndex)
+                        };
                     }
                     Data.Add(row);
                 }
             }
         }
 
-        public string[] Columns
+        public Column[] Columns
         {
             get
             {
@@ -90,12 +92,24 @@ namespace Connexion.DataExtensions
             }
         }
 
-        private string[] _columns;
+        private Column[] _columns;
         private int GetColumns(SqlDataReader reader)
         {
-            _columns = Enumerable.Range(0, reader.FieldCount).Select(reader.GetName).ToArray();
+            var fieldCount = reader.FieldCount;
 
-            return _columns.Length;
+            _columns = new Column[fieldCount];
+
+            for (int i = 0; i < fieldCount; i++)
+            {
+                _columns[i] = new Column {
+                    Index = i,
+                    Name = reader.GetName(i),
+                    Type = reader.GetFieldType(i)
+                };
+            }
+
+
+            return fieldCount;
         }
 
         private static void TryOpenConnection(SqlCommand command)
@@ -110,5 +124,16 @@ namespace Connexion.DataExtensions
         }
     }
 
+    public struct Column
+    {
+        public int Index;
+        public string Name;
+        public Type Type;
+    }
 
+    public struct ColumnData
+    {
+        public Column Column;
+        public object Data;   
+    }
 }

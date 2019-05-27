7e7ac07bf64b04ceac63693721b3b2fc8f2f1f15
Roman Wienicke <r.wienicke@connexion.at>
2019-05-27 15:41:56 +0200

    Performance Tests

---
+++ DataTable/Dictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connexion.DataExtensions
+{
+    public static class Dictionary
+    {
+        public static async Task<IDictionary<string, object>> FromSqlCommand(SqlCommand command)
+        {
+
+
+            try
+            {
+                if (command.Connection.State != System.Data.ConnectionState.Open)
+                {
+                    command.Connection.Open();
+                }
+
+                Dictionary<string, object> result = new Dictionary<string, object>();
+
+
+                SqlDataReader reader = await command.ExecuteReaderAsync();
+                bool dataIsPresent = reader.Read();
+
+                if (dataIsPresent)
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        result.Add(reader.GetName(i), reader[i]);
+                    }
+                }
+                reader.Close();
+
+                if (dataIsPresent) return result;
+                return null;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if (command.Connection.State != System.Data.ConnectionState.Closed)
+                {
+                    command.Connection.Close();
+                }
+            }
+        }
+    }
+}
--- DataTablePerformance/DataTablePerformance.cs
+++ DataTablePerformance/DataTablePerformance.cs
@@ -8,11 +8,18 @@ using System.Threading.Tasks;
 using SYSDATA = System.Data;
 using System.Data.SqlClient;
 using DataTableTests;
+using System.Collections;
 
 namespace Connexion.DataExtensions.Performance
 {
     public class DataTablePerformance
     {
+        const string sqlSelect = @"SELECT        c.id, c.contractId, c.name, c.groupName, c.description, c.TypeId, c.wageProgressID, c.digits, 
+			c.province, c.contractversion, c._validFor, c._weekLength, c._showInfo, c._coverage, c.version, c.isHidden, c.longName, 
+                         c.hasAnniversaryBonus, p.id AS Expr1, p.contractId AS Expr2, p.headline, p.text, p.areaID, p.version AS Expr3, p.isHidden AS Expr4
+FROM            kv.Contracts AS c INNER JOIN
+                         kv.Paragraphs AS p ON c.contractId = p.contractId";
+
         [Benchmark(Baseline = true)]
         public SYSDATA.DataTable SystemDataTableTest()
         {
@@ -20,7 +27,7 @@ namespace Connexion.DataExtensions.Performance
 
             using (var conn = new SqlConnection(context.ConnectionString))
             {
-                using (var command = new SqlCommand("select * from editor.Infos"))
+                using (var command = new SqlCommand(sqlSelect))
                 {
                     command.Connection = conn;
                     using (SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command))
@@ -41,7 +48,7 @@ namespace Connexion.DataExtensions.Performance
 
             using (var conn = new SqlConnection(context.ConnectionString))
             {
-                using (var command = new SqlCommand("select * from editor.Infos"))
+                using (var command = new SqlCommand(sqlSelect))
                 {
                     command.Connection = conn;
                     using (SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command))
@@ -54,21 +61,40 @@ namespace Connexion.DataExtensions.Performance
             }
         }
 
+        //[Benchmark()]
+        //public Connexion.DataExtensions.DataTable2 ConnexionDataTable2Test()
+        //{
+        //    var context = new TestDataContext();
+
+        //    using (var conn = new SqlConnection(context.ConnectionString))
+        //    {
+        //        using (var command = new SqlCommand("select * from editor.Infos"))
+        //        {
+        //            command.Connection = conn;
+        //            using (SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command))
+        //            {
+        //                return Connexion.DataExtensions.DataTable2.FromSqlCommand(command)
+        //                    .GetAwaiter().GetResult();
+        //            }
+        //        }
+
+        //    }
+
+        //}
+
         [Benchmark()]
-        public Connexion.DataExtensions.DataTable2 ConnexionDataTable2Test()
+        public IDictionary<string, object> TotallyDifferentTest()
         {
             var context = new TestDataContext();
 
             using (var conn = new SqlConnection(context.ConnectionString))
             {
-                using (var command = new SqlCommand("select * from editor.Infos"))
+                using (var command = new SqlCommand(sqlSelect))
                 {
                     command.Connection = conn;
-                    using (SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command))
-                    {
-                        return Connexion.DataExtensions.DataTable2.FromSqlCommand(command)
-                            .GetAwaiter().GetResult();
-                    }
+
+                    return Dictionary.FromSqlCommand(command).GetAwaiter().GetResult();
+
                 }
 
             }
